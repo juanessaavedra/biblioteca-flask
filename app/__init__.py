1f8f6c6cdf1ac652554a82d36bd3b0a4bb9701eb
juanessaavedra <juanessaavedra777@gmail.com>
2025-06-19 18:23:47 -0500

    Terminar de quitarJWT

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_jwt_extended import JWTManager
 from config import Config
 
 def create_app(config_class=Config):
